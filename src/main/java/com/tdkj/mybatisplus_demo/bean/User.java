b6acce3b6d82923cf315591a3aacbcafd3ec7115
simacunfu <674276553@qq.com>
2020-05-27 08:31:52 +0800

    12123123

---
+++ src/main/java/com/tdkj/mybatisplus_demo/bean/User.java
@@ -0,0 +1,22 @@
+package com.tdkj.mybatisplus_demo.bean;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.Data;
+
+import javax.validation.constraints.Email;
+//123123
+@Data
+@TableName("user")
+public class User {
+    @TableId(value = "id", type = IdType.AUTO)
+    Integer id;
+    @TableField(value = "name")
+    String name;
+    @TableField(value = "age")
+    Integer age;
+    @TableField(value = "email")
+    String email;
+}
